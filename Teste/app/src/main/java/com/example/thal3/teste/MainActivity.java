3337313010ba8c24caead1ad406d59c495f19b63
Thales <thal3s49@hotmail.com>
2018-10-30 22:26:54 -0300

    first commit

---
+++ Teste/app/src/main/java/com/example/thal3/teste/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.thal3.teste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button botest = new Button();
+        botest = findViewById(R.id.botest);
+
+
+    }
+
+
+}
